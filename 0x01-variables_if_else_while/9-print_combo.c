59cbad93983c525a03fee4d06cde9dd2ada7ba07
Jude-Ugoo <ugooeze0@gmail.com>
2023-09-08 10:53:40 -0700

    program that prints all possible combinations of single-digit numbers.
    Numbers must be separated by ,, followed by a space
    Numbers should be printed in ascending order

---
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - program starting point
+ *
+ * Return: end program
+ *
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar(10);
+	return (0);
+
+}
